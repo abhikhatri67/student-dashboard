862edd34dbe495a43ed91113656990418d9d52d5
abhikhatri67 <abhikhatri67@gmail.com>
2021-06-05 22:46:41 +0530

    Added links in nav and styled nav.

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -6,16 +6,25 @@ import { map, shareReplay } from 'rxjs/operators';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
+  navList = [
+    { title: 'Dashboard', iconUrl: 'assets/icons/dashboard.svg' },
+    { title: 'Materials', iconUrl: 'assets/icons/materials.svg' },
+    { title: 'Calendar', iconUrl: 'assets/icons/calendar.svg' },
+    { title: 'Transcripts', iconUrl: 'assets/icons/transcripts.svg' },
+    { title: 'Grades', iconUrl: 'assets/icons/grades.svg' },
+    { title: 'Attendance', iconUrl: 'assets/icons/attendence.svg' },
+    { title: 'Settings', iconUrl: 'assets/icons/settings.svg' },
+  ];
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
     .pipe(
-      map(result => result.matches),
+      map((result) => result.matches),
       shareReplay()
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
-
 }
